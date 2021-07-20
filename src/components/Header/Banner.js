3242a6cb4c713cc897fefd45ed46c0e01513e544
Ashraful Islam <ashrafunit7@gmail.com>
2021-07-21 03:46:17 +0600

    banner-responsiveness-fixed

--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -1,31 +1,40 @@
 import React from 'react';
-import banner from '../../images/banner.png'
 import seeker from '../../images/seeker.jpg'
 import './Banner.css';
 
 const Banner = () => {
     return (
         <div class='container background'>
-            <div class='row'>
+            <div class='row banner'>
                 <div class='col-md-6'>
                     <div class='dream-job'>
-                        <h1 class="color">Get Your Dream Job!</h1>
+                        <h1 style={{color: '#232763', fontWeight: 'bold'}}>Get Your Dream Job!</h1>
                     </div>
 
-                    <div class="input-group mb-3">
+                    <div class="input-group mb-5">
                         <input id='solid' type="text" class="form-control" placeholder="Search Your Job Here" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
                         <button type="button" class="btn-color">Search</button>
                     </div>
                     
                 </div>
 
-                <div class='col-md-6 job-button'>
-                    <div class='col-md-3 seeker'>
+                <div class='col-md-6 seeker-card'>
+                <div className='seeker'>
+                        <button type="button" class="seeker-button"><img class="seeker-employe" src={seeker} alt="job seeker image" width="100" height="100"/>JobSeeker</button>
+                    </div>
+                    <div className='employe'>
+                        <button type="button" class="seeker-button"><img class="seeker-employe" src={seeker} alt="job seeker image" width="100" height="100"/>Employe</button>
+                    </div>
+
+
+
+
+                    {/* <div class='col-md-3 seeker'>
                         <button type="button" class="seeker-button"><img class="seeker-employe" src={seeker} alt="job seeker image" width="100" height="100"/>JobSeeker</button>
                     </div>
                     <div class='col-md-3 employe'>
                         <button type="button" class="seeker-button"><img class="seeker-employe" src={seeker} alt="job seeker image" width="100" height="100"/>Employe</button>
-                    </div>
+                    </div> */}
                 </div>
 
                 
