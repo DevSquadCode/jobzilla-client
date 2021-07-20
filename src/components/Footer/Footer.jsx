b94da45074b299580e939000522da3ce3cc322b1
Jobayer-Majumder <jobayermojumder70@gmail.com>
2021-07-20 17:06:08 +0600

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import footerStyle from './footer.module.css'
+import logo from '../../images/Job-Exchange-Logo.png';
+import { 
+    followUs,
+    navigationOne, 
+    navigationTow, 
+    popularJobs 
+} from './footerData/FooterData';
 
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className='bg-secondary py-5'>
             <div className="container">
-                <h2 className={footerStyle.footer}>hello footer coming</h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <img className='img-fluid' src={logo} alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi molestias obcaecati sint vero cum adipisci eos officia nihil assumenda rem.</p>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="row">
+                            <div className="col-md-3">
+                                <h4>Navigation</h4>
+                                {
+                                    navigationOne.map((link, i) => <li key={i++} className='list-unstyled'>{link.title}</li>)
+                                }
+                            </div>
+                            <div className="col-md-3">
+                                {
+                                    navigationTow.map((link, i) => <li key={i++} className='list-unstyled'>{link.title}</li>)
+                                }
+                            </div>
+                            <div className="col-md-3">
+                                <h4>Follow Us</h4>
+                                {
+                                    followUs.map((link, i) => <li key={i++} className='list-unstyled'>{link.title}</li>)
+                                }
+                            </div>
+                            <div className="col-md-3">
+                                <h4>Popular Jobs</h4>
+                                {
+                                    popularJobs.map((job, i) => <li key={i++} className='list-unstyled'>{job.title}</li>)
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </footer>
     );
---
+++ src/components/Footer/footerData/FooterData.js
@@ -0,0 +1,77 @@
+export const navigationOne = [
+    {
+        title: 'Home',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Jobs',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Candidates',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Partners',
+        link: 'http://localhost:3000/'
+    },
+    
+]
+
+
+export const navigationTow = [
+    {
+        title: 'About Us',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Contact Us',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Terms & Conditions',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Privacy Policy',
+        link: 'http://localhost:3000/'
+    },
+]
+
+export const followUs = [
+    {
+        title : 'Blog',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Twitter',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Facebook',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Youtube',
+        link: 'http://localhost:3000/'
+    }
+];
+
+export const popularJobs = [
+    {
+        title: 'Web Developer',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Web Design',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'UI & UX Design',
+        link: 'http://localhost:3000/'
+    },
+    {
+        title: 'Account Manager',
+        link: 'http://localhost:3000/'
+    }
+]
