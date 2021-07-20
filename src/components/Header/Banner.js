8311cfc6fa5db94421e65a52bb0270df0b2b5b30
Towhidul1999 <towhidulsislam@gmail.com>
2021-07-20 16:26:11 +0600

    added-button

--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -13,14 +13,21 @@ const Banner = () => {
 
                     <div class="input-group mb-3">
                         <input id='solid' type="text" class="form-control" placeholder="Search Your Job Here" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
-                        <button type="button" class="btn-color">Primary</button>
+                        <button type="button" class="btn-color">Search</button>
                     </div>
                     
                 </div>
 
-                <div class='col-md-6'>
-                    <img class='job-image' src={banner} alt="Job Banner"></img>
+                <div class='col-md-6 job-button'>
+                    <div class='col-md-3 seeker'>
+                        <button type="button" class="seeker-button"><image src=""></image>I am Job Seeker</button>
+                    </div>
+                    <div class='col-md-3 employe'>
+                        <button type="button" class="seeker-button">I am Employe</button>
+                    </div>
                 </div>
+
+                
             </div>
         </div>
     );
