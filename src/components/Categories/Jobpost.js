cc6304f35216d19b36737db2165c7b1744f2a131
Towhidul1999 <towhidulsislam@gmail.com>
2021-07-24 16:39:56 +0600

    added-new-feature

--- src/components/Categories/Jobpost.js
+++ src/components/Categories/Jobpost.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './Jobpost.css';
+import companyImage from '../../images/companyImage.jpg'
 
 const Jobpost = () => {
     return (
         <div className='container card'>
+            <div className='row'>
+
             <h1 className="text-center">Recent Job Post</h1>
+
             <div className="col-md-12">
              <div class="card box-shadow">
               <div class="card-body">
                 {/* <h5 class="card-title">Special title treatment</h5> */}
                 <div className="icon">
-                    <i className="fa fa-code"></i>
+                    {/* <i className="fa fa-code"></i> */}
+                    <img src='#' alt="Girl in a jacket"></img>
                 </div>
                 <h4>Company Name</h4>
                 <p>Junior web Dev.</p>
@@ -23,7 +28,7 @@ const Jobpost = () => {
               <div class="card-body">
                 {/* <h5 class="card-title">Special title treatment</h5> */}
                 <div className="icon">
-                    <i className="fa fa-code"></i>
+                    <img src='#' alt="Girl in a jacket"></img>
                 </div>
                 <h4>Company Name</h4>
                 <p>Junior web Dev.</p>
@@ -36,14 +41,15 @@ const Jobpost = () => {
               <div class="card-body">
                 {/* <h5 class="card-title">Special title treatment</h5> */}
                 <div className="icon">
-                    <i className="fa fa-code"></i>
+                    <img src='#' alt="Girl in a jacket"></img>
                 </div>
                 <h4>Company Name</h4>
                 <p>Junior web Dev.</p>
               </div>
             </div>
           </div>
-
+            <small className='text-center'>Find More Jobs</small>
+          </div>
         </div>
     );
 };
