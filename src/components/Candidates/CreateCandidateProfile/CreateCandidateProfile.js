58bd37d46aeb862a1a4b386da209de30bfa766fd
Ashraful Islam <ashrafunit7@gmail.com>
2021-08-02 23:16:06 +0600

    form-refined

--- src/components/Candidates/CreateCandidateProfile/CreateCandidateProfile.js
+++ src/components/Candidates/CreateCandidateProfile/CreateCandidateProfile.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import './CeateCandidateProfile.css'
+import './CreateCandidateProfile.css'
 
 const CreateCandidateProfile = () => {
     return (
-        <div className="row m-3">
+        <div className="formField">
+            <div className="row ">
            <div className="col-md-2">
 
            </div>
 
            <div className="col-md-10">
            <form action="">
-               <div className="row d-flex align-items-center">
+               <div className="row m-3 d-flex align-items-center">
                  <div className="col-md-4">
-                        <h3>Basic Information:</h3>
+                        <h3><i class="fas fa-info-circle"></i> Basic Information:</h3>
                         <input className="name" type="text" name="" placeholder="Your Name" id="" required />
                         <br /><br />
                         <input className="email" type="email" placeholder="Your Email" name="" id="" required/>
@@ -26,7 +27,7 @@ const CreateCandidateProfile = () => {
                         <br /><br />
                         <input className="permanentAddress" type="text" placeholder="Permanent Address" />
                         <br /><br />
-                        <h4>Education:</h4>
+                        <h4><i class="fas fa-school"></i> Education:</h4>
                         <input className="educationLevel" type="text" placeholder="Education Level" />
                         <br /><br />
                         <input className="degree" type="text" placeholder="Degree Title" />
@@ -37,7 +38,7 @@ const CreateCandidateProfile = () => {
                  </div>
 
                  <div className="col-md-4">
-                     <h4>Job Experience:</h4>
+                     <h4><i class="fas fa-user-md"></i> Job Experience:</h4>
                      <input className="jobTitle" type="text" placeholder="Job Title"/>
                      <br /><br />
                      <input className="company" type="text" placeholder="Company"/>
@@ -46,17 +47,13 @@ const CreateCandidateProfile = () => {
                      <input className="startDate" type="date"/>
                      <h6 className="mt-2">End Date:</h6>
                      <input className="endDate" type="date"/>
-                     
-                     <br />
-                     <br />
-                     <h4>Skill Set:</h4>
+                     <br /><br />
+                     <h4><i class="fab fa-battle-net"></i> Skill Set:</h4>
                      <input className="skillName" type="text" placeholder="Skill Name"/>
                      <br /><br />
                      <input className="experience" type="number" placeholder="Experience in Year"/>
-
-                     <br />
-                     <br />
-                     <h4>Important Links:</h4>
+                     <br /> <br />
+                     <h4><i class="fas fa-link"></i> Important Links:</h4>
                      <input className="github" type="url" placeholder="Github" />
                      <br /><br />
                      <input className="linkedIn" type="url" placeholder="Linked In" />
@@ -65,7 +62,7 @@ const CreateCandidateProfile = () => {
                  </div>
 
                  <div className="col-md-4">
-                     <h4>Projects:</h4>
+                     <h4><i class="fas fa-tasks"></i> Projects:</h4>
                      <h5>Project-1:</h5>
                      <input className="projectField" type="text" placeholder="Project Name"/>
                      <br /> <br />
@@ -91,6 +88,7 @@ const CreateCandidateProfile = () => {
             </form>
            </div>
 
+        </div>
         </div>
     );
 };
