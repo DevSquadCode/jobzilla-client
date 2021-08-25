b458607f1704516a0878e7186c625a2c102f77c8
Jobayer-Majumder <jobayermojumder70@gmail.com>
2021-08-25 15:22:22 +0600

    fetchin search result and showing in modal

--- src/components/Header/banner/Banner.jsx
+++ src/components/Header/banner/Banner.jsx
@@ -6,13 +6,16 @@ import { locations, positions } from '../searchSuggestion/SearchSuggestionData.'
 
 const Banner = () => {
     const [modalShow, setModalShow] = React.useState(false);
-    const [searchText, setSearchText] = React.useState();
+    const [suggestText, setSuggestText] = React.useState();
+    const [inputText, setInputText] = React.useState();
     const [suggest, setSuggest] = React.useState([]);
     const [resFound, setResFound] = React.useState(true);
+    const [searchData, setSearchData] = React.useState([]);
+    const [searchError, setSearchError] = React.useState();
 
 
-    const handleShowModal= () => {
-      setModalShow(true);
+    const handleShowModal = () => {
+        setModalShow(true);
     };
 
 
@@ -20,91 +23,108 @@ const Banner = () => {
         const value = e.target.value;
         const name = e.target.name;
         let suggestion = [];
-        if(name === 'jobPosition'){
+        if (name === 'jobPosition') {
             if (value.length > 0) {
                 suggestion = positions
-                  .sort()
-                  .filter((e) => e.toLowerCase().includes(value.toLowerCase()));
+                    .sort()
+                    .filter((e) => e.toLowerCase().includes(value.toLowerCase()));
                 setResFound(suggestion.length !== 0 ? true : false);
-              };
-            setSuggest({position: suggestion})
+            };
+            setSuggest({ position: suggestion })
+            const newValue = { ...inputText }
+            newValue[name] = value;
+            setInputText(newValue)
         }
-        if(name === 'jobLocation'){
+        if (name === 'jobLocation') {
             if (value.length > 0) {
                 suggestion = locations
-                  .sort()
-                  .filter((e) => e.toLowerCase().includes(value.toLowerCase()));
+                    .sort()
+                    .filter((e) => e.toLowerCase().includes(value.toLowerCase()));
                 setResFound(suggestion.length !== 0 ? true : false);
-            ;}
-            setSuggest({location: suggestion});
+                ;
+            }
+            setSuggest({ location: suggestion });
+            const newValue = { ...inputText }
+            newValue[name] = value;
+            setInputText(newValue)
         }
-        
-        setSearchText(value);
+        setSuggestText(value);
     };
-    console.log(searchText)
+
     const suggestedText = (value) => {
-        const prevSearch = {...searchText, value}
-        setSearchText(prevSearch);
+        const prevSearch = { ...suggestText, value }
+        setSuggestText(prevSearch);
         setSuggest([]);
-      };
-
-      const getPositionSuggest = () => {
-        if (suggest.length === 0 && searchText !== "" && !resFound) {
-          return <p className='position-absolute'>Search Content Not Found</p>;
-        }
-        return (
-          <>
-            <div className="position-absolute bg-light w-75 p-3 rounded" style={{zIndex: '1'}}>
-                {suggest?.position?.map((item, index) => {
-                return (
-                    <div onClick={() => suggestedText({position: item})} key={index} className='' style={{cursor: 'pointer'}}>
-                        <p>{item}</p>
-                        {index !== suggest.length - 1 && <hr />}
-                    </div>
-                );
-                })}
-            </div>
-          </>
-        );
-      };
-
-      const getLocationSuggest = () => {
-        if (suggest.length === 0 && searchText !== "" && !resFound) {
-          return <p className='position-absolute'>Search Content Not Found</p>;
-        }
-        return (
-          <>
-            <div className="position-absolute bg-light w-75 p-3 rounded" style={{zIndex: '1'}}>
-                {suggest?.location?.map((item, index) => {
-                return (
-                    <div onClick={() => suggestedText({location: item})} key={index} className='' style={{cursor: 'pointer'}}>
-                        <p>{item}</p>
-                        {index !== suggest.length - 1 && <hr />}
-                    </div>
-                );
-                })}
-            </div>
-          </>
-        );
-      };
+    };
 
     const handleSearch = e => {
-        e.preventDefault(); 
-        handleShowModal()
+        e.preventDefault();
+
+        if(inputText?.jobPosition || inputText?.jobLocation){
+            handleShowModal();
+        };
+        
+        fetch('http://localhost:8080/searching', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(inputText)
+        }).then(res => res.json()).then(data => setSearchData(data)).catch(err => setSearchError(err));
     };
 
+    console.log(inputText)
+
+    const getPositionSuggest = () => {
+        if (suggest?.position?.length === 0 && suggestText !== "" && !resFound) {
+            return <p className='position-absolute py-2'>Search Content Not Found</p>;
+        }
+        return (
+            <>
+                <div className="position-absolute bg-light w-75 p-3 rounded" style={{ zIndex: '1' }}>
+                    {suggest?.position?.map((item, index) => {
+                        return (
+                            <div onClick={() => suggestedText({ position: item })} key={index} className='' style={{ cursor: 'pointer' }}>
+                                <p>{item}</p>
+                                {index !== suggest.length - 1 && <hr />}
+                            </div>
+                        );
+                    })}
+                </div>
+            </>
+        );
+    };
+
+    const getLocationSuggest = () => {
+        if (suggest?.location?.length === 0 && suggestText !== "" && !resFound) {
+            return <p className='position-absolute py-2'>Search Content Not Found</p>;
+        }
+        return (
+            <>
+                <div className="position-absolute bg-light w-75 p-3 rounded" style={{ zIndex: '1' }}>
+                    {suggest?.location?.map((item, index) => {
+                        return (
+                            <div onClick={() => suggestedText({ location: item })} key={index} className='' style={{ cursor: 'pointer' }}>
+                                <p>{item}</p>
+                                {index !== suggest.length - 1 && <hr />}
+                            </div>
+                        );
+                    })}
+                </div>
+            </>
+        );
+    };
 
     return (
         <section className='py-4'>
             <div className="container mt-5 py-5">
+            {/* {searchError && alert(searchError.message)} */}
                 <div className="row py-3">
                     <div className="col-md-4 py-2 position-relative">
-                        <input onChange={handleInputChange} value={searchText?.value?.position} name='jobPosition' className='form-control' type="search" placeholder='Job Position' />
-                        {searchText && getPositionSuggest()}
+                        <input onChange={handleInputChange} value={suggestText?.value?.position} name='jobPosition' className='form-control' type="search" placeholder='Job Position' />
+                        {suggestText && getPositionSuggest()}
                     </div>
                     <div className="col-md-4 py-2">
-                        <input onChange={handleInputChange} value={searchText?.value?.location} name='jobLocation' className='form-control' type="search" placeholder='Job Location' />
-                        {searchText && getLocationSuggest()}
+                        <input onChange={handleInputChange} value={suggestText?.value?.location} name='jobLocation' className='form-control' type="search" placeholder='Job Location' />
+                        {suggestText && getLocationSuggest()}
                     </div>
                     <div className="col-md-4 py-2">
                         <div className="input-group">
@@ -115,7 +135,7 @@ const Banner = () => {
                                 <option value="3">Part Time</option>
                             </select>
                             <button onClick={handleSearch} className="btn-brand">Search</button>
-                            <CustomModal setShow={setModalShow} show={modalShow}/>
+                            <CustomModal searchData={searchData} setShow={setModalShow} show={modalShow} />
                         </div>
                     </div>
                 </div>
--- src/components/Shared/Modal/CustomModal.jsx
+++ src/components/Shared/Modal/CustomModal.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
+import logo from '../../../images/companyImage.jpg';
 
 
 
+const CustomModal = ({ setShow, show, searchData }) => {
 
-const CustomModal = ({setShow, show}) => {
-    
-    return (
-      <>
-        <Modal show={show} fullscreen={true} onHide={() => setShow(false)}>
-          <Modal.Header closeButton className='bg-light border'>
-            <h5>Available Jobs</h5>
-          </Modal.Header>
-          <Modal.Body>
-                <h5>Your Available Jobs <span className="text-success">
-                    {/* {inputValue?.jobPosition && `on position ${inputValue?.jobPosition}`} {inputValue?.jobLocation && `in ${inputValue?.jobLocation}`}   */}
-                  </span>
-                </h5>
-          </Modal.Body>
-        </Modal>
-      </>
-    );
+  console.log(searchData);
+
+  return (
+    <>
+      <Modal show={show} fullscreen={true} onHide={() => setShow(false)}>
+        <Modal.Header closeButton className='bg-light border'>
+          <h5>Available Jobs</h5>
+        </Modal.Header>
+        <Modal.Body>
+          {
+            searchData?.map((data, index) => <div key={index++} className='container'>
+              <div className="row">
+                <div className="p-1 col-md-6">
+                  <h4>{data.title}</h4>
+                  <h6>{data.name}</h6>
+                </div>
+                <div className="col-md-6">
+                  <img src={logo} alt="company logo" />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-3">
+                  <h5>Start Date</h5>
+                  <p>Immediately</p>
+                </div>
+                <div className="col-md-3">
+                  <h5>Salary</h5>
+                  <p>20k BDT</p>
+                </div>
+                <div className="col-md-3">
+                  <h5>Apply by</h5>
+                  <p>{data.deadline}</p>
+                </div>
+              </div>
+            </div>)
+          }
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 };
 
 export default CustomModal;
