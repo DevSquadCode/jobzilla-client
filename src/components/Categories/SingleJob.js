04bb55e02ff56a617a0eb719afc196e0dd973d54
Jobayer-Majumder <jobayermojumder70@gmail.com>
2021-08-25 18:02:48 +0600

    resolved conflict

---
+++ src/components/Categories/SingleJob.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SingleJob = (props) => {
+
+    const {name, title, jobType, deadline} = props.singleJob;
+    console.log(props.singleJob)
+    return (
+        <div class="cardList">
+              <div class="cardList-body">
+                <h3>{name}</h3>
+                <h5 style={{fontWeight: 400}}>{title}</h5>
+                <p>{jobType}</p>
+              </div>
+              <div className="card-apply text-center">
+                <h5>Apply Now</h5>
+                <p>Dead-line: {deadline}</p>
+              </div>
+            </div>
+    );
+};
+
+export default SingleJob;
